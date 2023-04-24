16de708c9eefbf3fba73282a69c78833e10593bd
Lamchi <57172718+Lamchi-Joo@users.noreply.github.com>
2023-04-24 17:55:46 +0800

    Add files via upload

--- request/__init__.py
+++ request/__init__.py
@@ -1,1 +1,21 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
 
+from .curl_async_http_client import CurlAsyncHTTPClient
+
+
+class RequestWrapper(object):
+
+    def __init__(self):
+        pass
+
+    @staticmethod
+    async def fetch(*args, **kwargs):
+        fetcher = CurlAsyncHTTPClient(force_instance=True)
+        try:
+            res = await fetcher.fetch(*args, **kwargs)
+            fetcher.close()
+        except Exception as e:
+            fetcher.close()
+            raise e
+        return res
