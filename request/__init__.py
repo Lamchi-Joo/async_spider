f7850783d42f071bbb0b28f94a4534dc5aaea9fc
linzhi.zhu <11788683@qq.com>
2023-04-24 17:36:04 +0800

    update async_spider

--- request/__init__.py
+++ request/__init__.py
@@ -1,1 +1,26 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
 
+from .curl_async_http_client import CurlAsyncHTTPClient
+
+
+class RequestWrapper(object):
+    """
+    Fetcher wrapper class of loginer
+    """
+
+    def __init__(self):
+        pass
+
+    @staticmethod
+    async def fetch(*args, **kwargs):
+        """Wrap fetcher
+        """
+        fetcher = CurlAsyncHTTPClient(force_instance=True)
+        try:
+            res = await fetcher.fetch(*args, **kwargs)
+            fetcher.close()
+        except Exception as e:
+            fetcher.close()
+            raise e
+        return res
---
+++ sample/baidu.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+    baidu.py
+    ~~~~~~~~~~~
+
+    :author: Lamchi
+    :copyright: (c) 2023, qizhidao
+    :date created: 2023-04-24
+    :python version: 3.7.3
+"""
+from sample.base_spider import BaseSpider
+
+
+class Spider(BaseSpider):
+
+    name = "百度"
+    encoding = "utf-8"
+    concurrent_limit = 100
+    RETRY_TIME_DELAY = 0.01
+    MAX_RETRY_COUNT = 2
+
+    urls = [
+        "https://baidu.com" for i in range(100)
+    ]
+
+    async def start_request(self, url):
+        resp = await self.aio_request(
+            url,
+            auto_proxy=False,  # 自动添加代理
+            auto_ua=False,  # 自动添加请求头
+            follow_redirects=True  # 重定向
+        )
+        await self.logger.info(resp.code)
+        text = self.decode_body(resp)
+        print(text)
+
+
+if __name__ == '__main__':
+    spider = Spider()
+    spider.run()
