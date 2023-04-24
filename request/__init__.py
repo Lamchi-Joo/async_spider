ba4f97eb5fc2606a84975adcb03096471391dc6b
linzhi.zhu <11788683@qq.com>
2023-04-24 17:36:31 +0800

    update async_spider

--- request/__init__.py
+++ request/__init__.py
@@ -5,17 +5,12 @@ from .curl_async_http_client import CurlAsyncHTTPClient
 
 
 class RequestWrapper(object):
-    """
-    Fetcher wrapper class of loginer
-    """
 
     def __init__(self):
         pass
 
     @staticmethod
     async def fetch(*args, **kwargs):
-        """Wrap fetcher
-        """
         fetcher = CurlAsyncHTTPClient(force_instance=True)
         try:
             res = await fetcher.fetch(*args, **kwargs)
